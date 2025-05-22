a8e97248a4bdec3adbda606fcdc846e4c3981b19
hubvoy <wojewoda7@protonmail.com>
2025-05-22 14:53:35 +0200

    Initial commit of clean quantum-eps project

---
+++ searchep/__init__.py
@@ -0,0 +1,26 @@
+from . import (
+    data,
+    eval,
+    GPFlow_model_class,
+    gpr,
+    main,
+    matrix,
+    plots,
+    tests,
+    training,
+    zero_search,
+)
+
+__all__ = [
+    "data",
+    "eval",
+    "GPFlow_model_class",
+    "gpr",
+    "main",
+    "matrix",
+    "plots",
+    "tests",
+    "training",
+    "zero_search",
+]
+__version__ = "0.1.0"
